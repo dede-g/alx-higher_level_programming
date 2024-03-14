15de26fd758c0b1aadddaec466c0098190ac2cb2
dede-g <dedeteddy3@gmail.com>
2024-03-14 00:30:22 -0700

    Rewrite code

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if letter != 101 and letter != 113:
-        print("{}".format(chr(letter)), end="")
+for ch in range(97, 123):
+    if (ch != 101) and (ch != 113):
+        print("{:c}".format(ch), end="")
